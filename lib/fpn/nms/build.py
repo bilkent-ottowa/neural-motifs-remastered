36ecc8e71ba0a75fb3daaec06bdcadc6893127ce
nayanua <yigityldrm06@gmail.com>
2024-11-03 10:09:43 +0300

    Fixing nms to current PyTorch API

--- lib/fpn/nms/build.py
+++ lib/fpn/nms/build.py
@@ -1,35 +1,44 @@
+from setuptools import setup, Distribution
 import os
 import torch
-from torch.utils.cpp_extension import BuildExtension as create_extension
+from torch.utils.cpp_extension import BuildExtension, CppExtension, CUDAExtension
+
 # Might have to export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
 
 sources = []
 headers = []
 defines = []
 with_cuda = False
+this_file = os.path.dirname(os.path.realpath(__file__))
 
 if torch.cuda.is_available():
     print('Including CUDA code.')
-    sources += ['src/nms_cuda.c']
-    headers += ['src/nms_cuda.h']
+    sources += [os.path.join(this_file, 'src/nms_cuda.cpp')]
+    headers += [os.path.join(this_file, 'src/nms_cuda.h')]
     defines += [('WITH_CUDA', None)]
     with_cuda = True
 
-this_file = os.path.dirname(os.path.realpath(__file__))
+
 print(this_file)
 extra_objects = ['src/cuda/nms.cu.o']
 extra_objects = [os.path.join(this_file, fname) for fname in extra_objects]
 
-ffi = create_extension(
-    '_ext.nms',
-    headers=headers,
-    sources=sources,
-    define_macros=defines,
-    relative_to=__file__,
-    with_cuda=with_cuda,
-    extra_objects=extra_objects
+setup(
+    name='my_extension',
+    ext_modules=[
+        CUDAExtension(
+            name='my_extension',
+            sources=sources,
+            extra_objects=extra_objects,
+            define_macros=defines,
+            include_dirs=[os.path.join(this_file, 'src')],
+            extra_compile_args={
+                'cxx': ['-std=c++17', '-D_GLIBCXX_USE_CXX11_ABI=0'],
+                'nvcc': ['-std=c++14']
+            }
+        )
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    }
 )
-
-if __name__ == '__main__':
-    ffi.build()
-
---
+++ lib/fpn/nms/src/nms_cuda.cpp
@@ -0,0 +1,23 @@
+#include <math.h>
+#include "cuda/nms_kernel.h"
+
+#include <ATen/ATen.h>
+#include <ATen/cuda/CUDAContext.h>
+#include <ATen/cuda/CUDAEvent.h>
+#include <c10/cuda/CUDAStream.h>
+#include <c10/util/SmallVector.h>
+
+cudaStream_t stream = c10::cuda::getCurrentCUDAStream();
+
+int nms_apply(at::Tensor keep, at::Tensor boxes_sorted, const float nms_thresh)
+{
+    int* keep_data = keep.data_ptr<int>();
+    const float* boxes_sorted_data = boxes_sorted.data_ptr<float>();
+
+    const int boxes_num = boxes_sorted.size(0);
+
+    const int devId = boxes_sorted.get_device();
+
+    int numTotalKeep = ApplyNMSGPU(keep_data, boxes_sorted_data, boxes_num, nms_thresh, devId);
+    return numTotalKeep;
+}
